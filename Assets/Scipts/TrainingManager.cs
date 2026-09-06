3ead7fdf3903683739bf7ec6595a991d8b7efaa9
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-09-06 17:18:09 +0200

    taining

---
+++ Assets/Scipts/TrainingManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TrainingManager : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class CPRStep
+    {
+        public string stepTitle;
+        public string character;    
+        public string triggerName;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
